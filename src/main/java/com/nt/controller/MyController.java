543b58d3d22f13c092540e817858cbc1fd138039
BhupatiBm <63635039+BhupatiBm@users.noreply.github.com>
2020-06-07 16:36:43 +0530

    Update MyController.java

--- src/main/java/com/nt/controller/MyController.java
+++ src/main/java/com/nt/controller/MyController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
+
 	@RequestMapping("/welcome.htm")
 	public String getWelcomePage() {
+		System.out.println("Welcome");
 		return "welcome";
 	}
 
